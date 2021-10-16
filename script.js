ea309ed81347ee8e87df1a2bf57f9a018cb5cb32
edavetisyan <edmondavetisyanw@gmail.com>
2021-10-16 20:25:09 +0300

    save

--- script.js
+++ script.js
@@ -195,3 +195,27 @@
 
 // console.log(explode("312"));
 // console.log(explode("102269"));
+
+// function formatDuration (seconds) {
+//   let time = { year: 31536000, day: 86400, hour: 3600, minute: 60, second: 1 },
+//       result = [];
+
+//   if (seconds === 0) return 'now';
+  
+//   for (var key in time) {
+//     if (seconds >= time[key]) {
+//       var val = Math.floor(seconds/time[key]);
+//       // result.push(val += val > 1 ? ' ' + key + 's' : ' ' + key);
+//       if (val>1) {
+//          result.push(val+= ' ' + key + 's')
+//       }
+//       else{
+//         result.push(val+= ' ' + key)
+        
+//       }
+//       seconds = seconds % time[key];
+//     }
+//   }
+ 
+//   return result.length > 1 ? result.join(', ').replace(/,([^,]*)$/,' and'+'$1') : result[0]
+// }
